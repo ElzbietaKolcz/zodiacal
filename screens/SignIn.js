024bd93de92e7e878e814f1a2721d061402820dc
ek <elzbieta.kolcz.13@gmail.com>
2023-10-19 23:31:20 +0200

    Connect to firebase

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
-import * as React from "react";
+import React, { useEffect } from "react";
 import {
   KeyboardAvoidingView,
   Image,
@@ -9,22 +9,30 @@ import {
 } from "react-native";
 import images from "../images";
 import { TextInput } from "react-native-paper";
-import tw from 'twrnc';
-import useAuth from "../hooks/useAuth";
+import tw from "twrnc";
+import { auth } from "../firebase";
 
 
-
-
-const SignIn = () =>{
+const SignIn = () => {
   const navigation = useNavigation();
-  const{user} =useAuth();
 
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const signIn = () =>{
 
-  }
- 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        navigation.replace("Home");
+      }
+    });
+
+    return () => {
+      // Clean up the subscription when the component unmounts
+      unsubscribe();
+    };
+  }, []);
+
+  const signIn = () => {};
 
   return (
     <KeyboardAvoidingView style={tw`flex-1`}>
@@ -34,14 +42,14 @@ const SignIn = () =>{
           source={images.bgSignIn}
           resizeMode="cover"
         ></Image>
-  
+
         <View style={tw`flex-row justify-around w-full absolute`}>
           <Image
             style={tw`h-[60] w-[45] mt-16`}
             source={images.logo}
           />
         </View>
-  
+
         <View style={tw`h-full w-full flex justify-around mt-34 absolute`}>
           <View style={tw`flex items-center mx-8 `}>
             <View style={tw`w-full`}>
@@ -61,29 +69,33 @@ const SignIn = () =>{
                 secureTextEntry
               />
             </View>
-  
+
             <View style={tw`w-full`}>
-              <Pressable onPress={signIn} style={tw`rounded-full p-4 mb-3 mt-8 bg-[#9C27B0]`}>
+              <Pressable
+                onPress={signIn}
+                style={tw`rounded-full p-4 mb-3 mt-8 bg-[#9C27B0]`}
+              >
                 <Text style={tw`text-center text-lg uppercase text-white`}>
                   Sign in
                 </Text>
               </Pressable>
             </View>
-  
+
             <View style={tw`flex-col justify-center mt-4`}>
-              <Text style={tw`text-center text-lg`}>
-                {" "}
-                - Or sign in with -{" "}
-              </Text>
+              <Text style={tw`text-center text-lg`}> - Or sign in with - </Text>
               <View style={tw`flex-row my-3 mb-6 `}>
-                <Pressable style={tw`flex-row rounded-full mx-4 bg-white p-4 mb-3 mt-5`}>
+                <Pressable
+                  style={tw`flex-row rounded-full mx-4 bg-white p-4 mb-3 mt-5`}
+                >
                   <Image
                     style={tw`h-[8] w-[8]`}
                     source={images.google}
                   />
                   <Text style={tw`p-2 `}> Google </Text>
                 </Pressable>
-                <Pressable style={tw`flex-row rounded-full mx-4 bg-white p-4 mb-3 mt-5`}>
+                <Pressable
+                  style={tw`flex-row rounded-full mx-4 bg-white p-4 mb-3 mt-5`}
+                >
                   <Image
                     style={tw`h-[8] w-[6.5]`}
                     source={images.apple}
@@ -91,14 +103,16 @@ const SignIn = () =>{
                   <Text style={tw`p-2 `}> Apple </Text>
                 </Pressable>
               </View>
-  
+
               <View style={tw`flex-row justify-center`}>
                 <Text style={tw`text-center text-lg`}>
                   {" "}
                   Don't have an account?
                 </Text>
                 <Pressable onPress={() => navigation.navigate("SignUp")}>
-                  <Text style={tw`text-center text-lg font-semibold px-2 text-[#9C27B0]`}>
+                  <Text
+                    style={tw`text-center text-lg font-semibold px-2 text-[#9C27B0]`}
+                  >
                     Sign up
                   </Text>
                 </Pressable>
@@ -109,6 +123,6 @@ const SignIn = () =>{
       </View>
     </KeyboardAvoidingView>
   );
-}
+};
 
 export default SignIn;
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -9,23 +9,39 @@ import {
 } from "react-native";
 import images from "../images";
 import { TextInput } from "react-native-paper";
-import tw from 'twrnc';
-import useAuth from "../hooks/useAuth";
+import tw from "twrnc";
+import { getAuth, createUserWithEmailAndPassword, updateProfile  } from "firebase/auth";
 
-
-
-
-const SignIn = () =>{
+const SignIn = () => {
   const navigation = useNavigation();
-  const{user} =useAuth();
 
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [name, setName] = React.useState("");
-  const signUp = () =>{
+  const [username, setUsername] = React.useState("");
 
-  }
- 
+  const handleSignUp = () => {
+    const auth = getAuth();
+
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        
+        updateProfile(user, { username })
+          .then(() => {
+            console.log("Ustawiono nazwę użytkownika:", username);
+          })
+          .catch((error) => {
+            console.error("Błąd podczas ustawiania nazwy użytkownika:", error);
+          });
+
+        console.log("Utworzono użytkownika:", user);
+        navigation.replace("SignIn");
+      })
+      .catch((error) => {
+        console.error("Błąd podczas tworzenia użytkownika:", error);
+      });
+  };
 
   return (
     <KeyboardAvoidingView style={tw`flex-1`}>
@@ -35,22 +51,22 @@ const SignIn = () =>{
           source={images.bgSignUp}
           resizeMode="cover"
         ></Image>
-  
+
         <View style={tw`flex-row justify-around w-full absolute`}>
           <Image
             style={tw`h-[60] w-[45] mt-16`}
             source={images.logo}
           />
         </View>
-  
+
         <View style={tw`h-full w-full flex justify-around mt-25 absolute`}>
           <View style={tw`flex items-center mx-8 `}>
-          <View style={tw`w-full`}>
+            <View style={tw`w-full`}>
               <TextInput
                 style={tw`bg-fuchsia-100/80 rounded-lg my-2`}
-                label="Name"
-                value={name}
-                onChangeText={(text) => setName(text)}
+                label="Username"
+                value={username}
+                onChangeText={(text) => setUsername(text)}
               />
             </View>
             <View style={tw`w-full`}>
@@ -70,24 +86,27 @@ const SignIn = () =>{
                 secureTextEntry
               />
             </View>
-            
+
             <View style={tw`w-full`}>
-              <Pressable raised onPress={signUp} style={tw`rounded-full p-4 mb-3 mt-8 bg-[#9C27B0]`}>
+              <Pressable onPress={() => handleSignUp(username)}
+                style={tw`rounded-full p-4 mb-3 mt-8 bg-[#9C27B0]`}
+              >
                 <Text style={tw`text-center text-lg uppercase text-white`}>
                   Sign up
                 </Text>
               </Pressable>
             </View>
-  
+
             <View style={tw`flex-col justify-center`}>
-  
               <View style={tw`flex-row justify-center`}>
                 <Text style={tw`text-center text-lg mt-10`}>
                   {" "}
                   Do you have an account?
                 </Text>
                 <Pressable onPress={() => navigation.navigate("SignIn")}>
-                  <Text style={tw`text-center text-lg font-semibold px-2 mt-10 text-[#9C27B0]`}>
+                  <Text
+                    style={tw`text-center text-lg font-semibold px-2 mt-10 text-[#9C27B0]`}
+                  >
                     Log in
                   </Text>
                 </Pressable>
@@ -98,6 +117,6 @@ const SignIn = () =>{
       </View>
     </KeyboardAvoidingView>
   );
-}
+};
 
 export default SignIn;
